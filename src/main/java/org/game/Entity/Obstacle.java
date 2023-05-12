2d9910d6309378739189b3ccdb2895015a46a5b0
shorivar2020 <101527748+shorivar2020@users.noreply.github.com>
2023-05-12 10:29:06 +0200

    Graphics of Building

--- src/main/java/org/game/Entity/Obstacle.java
+++ src/main/java/org/game/Entity/Obstacle.java
@@ -1,18 +1,32 @@
 package org.game.Entity;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Obstacle extends Entity{
+    BufferedImage image;
     public Obstacle(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        setImage(this);
+    }
+
+    public void setImage(Obstacle o){
+        try {
+            o.image = ImageIO.read(getClass().getResourceAsStream("/brics.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void draw(Graphics2D g2d) {
-        g2d.setColor(Color.lightGray);
-        g2d.fillRect(x, y, width, height);
+//        g2d.setColor(Color.lightGray);
+//        g2d.fillRect(x, y, width, height);
+        g2d.drawImage(image, x, y, null);
     }
 
     public Rectangle getBounds() {
